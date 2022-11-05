f0a8fd16d320abecf2989b7df9b946bcf39103e6
Chi3ffan <116543408+MarvHetz@users.noreply.github.com>
2022-11-05 13:15:48 +0100

    auslesen funktion fertig geschreiben

--- Dateiverarbeitung/WindowsFormsAppReise/Verwaltung.cs
+++ Dateiverarbeitung/WindowsFormsAppReise/Verwaltung.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace WindowsFormsAppReise
 {
     internal class Verwaltung
     {
-        private List<Reise> reisen;
+        private Reise reisen;
         public Verwaltung()
         {
-            reisen = new List<Reise>();
+            
         }
         
-        internal List<Reise> Reisen { get => reisen; set => reisen = value; }
+        internal Reise Reisen { get => reisen; set => reisen = value; }
 
         public List<Mensch> neuerTeilnehmer(Reise temp, string name, string email, string nachname)
         {
@@ -29,7 +31,23 @@ namespace WindowsFormsAppReise
 
         public void auslesen()
         {
-            
+            try
+            {
+                StreamReader sr = new StreamReader("Teilnehmer.xml");
+                XmlSerializer serializer = new XmlSerializer(typeof(Mensch));
+
+                List<Mensch> menschen = (List<Mensch>)serializer.Deserialize(sr);
+                
+                sr.Close();
+                
+                reisen = new Reise(new DateTime(2023,1,1),20,320,new DateTime(2023,3,1),"Kuba");
+                reisen.Teilnehmer = menschen;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw new Exception();
+            }
         }
     }
 }
