d7d048014d4123b8858d68bbb2695faced41c824
MarvHetz <hetzermarvin@dv-schulen.de>
2022-12-20 10:33:27 +0100

    lajdsljdfölaskfjdaslkjflöasjkf

---
+++ WPF/Ein und Ausgaben/Tier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ein_und_Ausgaben
+{
+    public class Tier
+    {
+        private string name;
+
+        public Tier()
+        {
+            name = "Garret";
+        }
+
+        public string Name { get => name; set => name = value; }
+
+        public string Message()
+        {
+            name = "Dante";
+            return name + " : Salve";
+        }
+    }
+}
---
+++ WPF/ZinsRechner/ZinsesZins.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZinsRechner
+{
+    public class ZinsesZins
+    {
+        private double kapital;
+        private double zinssatz;
+        private double laufzeit;
+        private double endKapital;
+
+        public ZinsesZins()
+        { }
+
+        public double Kapital { get => kapital; set => kapital = value; }
+        public double Zinssatz { get => zinssatz; set => zinssatz = value; }
+        public double Laufzeit { get => laufzeit; set => laufzeit = value; }
+        public double EndKapital { get => endKapital; set => endKapital = value; }
+    }
+}
