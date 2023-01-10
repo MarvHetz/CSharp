f911366634d4376b2de443abc792f2681d05b974
MarvHetz <hetzermarvin@dv-schulen.de>
2023-01-10 10:09:02 +0100

    FPAVorbereitung hinzugefügt

---
+++ WpfAppApothekeVorlage/WpfAppApotheke/WpfAppApotheke/Kauf.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+
+
+namespace WpfAppApotheke
+{
+    public class Kauf
+    {
+        private ObservableCollection<Medikament> alleMedikamente;
+        private ObservableCollection<Medikament> auswahlMedikamente;
+
+
+        public decimal Gesamtpreis { get => gesamtpreis;
+            set  { gesamtpreis = value;   }
+        }
+
+        public ObservableCollection<Medikament> AlleMedikamente { get => alleMedikamente; set => alleMedikamente = value; }
+        public ObservableCollection<Medikament> AuswahlMedikamente { get => auswahlMedikamente; set => auswahlMedikamente = value; }
+        public delegate void RezeptVorhanden();
+        public RezeptVorhanden OnRezeptVorhanden = null;
+
+        private decimal gesamtpreis;
+
+
+        public Kauf ()
+        {
+            Gesamtpreis = 0;
+            AlleDeserialisieren();
+            auswahlMedikamente = new ObservableCollection<Medikament>();
+        }
+
+        public void MedikamentHinzufuegen(Medikament medikament, bool kassenrezept, bool privatrezept)
+        {
+            if (medikament.Rezeptpflichtig)
+            {
+                if (!kassenrezept && !privatrezept)
+                {
+                    if (OnRezeptVorhanden != null)
+                    {
+                        OnRezeptVorhanden();
+                    }
+                }
+                else
+                {
+                    Gesamtpreis += medikament.ZuzahlungBerechnen(kassenrezept);
+                    auswahlMedikamente.Add(medikament);
+                }
+            }
+            else
+            {
+                Gesamtpreis += medikament.ZuzahlungBerechnen(kassenrezept);
+                auswahlMedikamente.Add(medikament);
+            }
+        }
+
+        public decimal Kaufen()
+        {
+            return gesamtpreis;
+        }
+
+        public ObservableCollection<Medikament> AlleDeserialisieren()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ObservableCollection<Medikament>));
+            StreamReader reader = new StreamReader("Auswahl.xml");
+            alleMedikamente = (ObservableCollection<Medikament>)serializer.Deserialize(reader);
+            return alleMedikamente;
+        }
+    }
+}
---
+++ WpfAppApothekeVorlage/WpfAppApotheke/WpfAppApotheke/Medikament.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppApotheke
+{
+    public class Medikament
+    {
+        private bool rezeptpflichtig;
+        private string name;
+        private decimal preis;
+        private decimal zuzahlung;
+
+        public Medikament()
+        { }
+        public Medikament(bool rezeptpflichtig, string name, decimal preis)
+        {
+            this.rezeptpflichtig = rezeptpflichtig;
+            this.name = name;
+            this.preis = preis;
+        }
+
+        public bool Rezeptpflichtig { get => rezeptpflichtig;  set => rezeptpflichtig = value; }
+        public string Name { get => name; set => name = value; }
+        public decimal Preis { get => preis;  set => preis = value; }
+        public decimal Zuzahlung { get => zuzahlung;  set => zuzahlung = value; }
+
+        public decimal ZuzahlungBerechnen(bool kassenrezept)
+        {
+            decimal Zuzahlung = preis;
+            if (kassenrezept)
+            {
+                Zuzahlung = Convert.ToDecimal(Convert.ToDouble(preis) * 0.1);
+                if (Zuzahlung < 5)
+                {
+                    Zuzahlung = 5;
+                }
+                else if (Zuzahlung > 10)
+                {
+                    Zuzahlung = 10;
+                }
+            }
+            return Zuzahlung;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
